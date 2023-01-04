67f7950d0bf3a54b23cb5f0bdaaf6f51b2557490
jerenteria <renteria.juan96@gmail.com>
2023-01-03 21:32:27 -0800

    add expense input bar now resets after user submits forminstead of reloading page we keep using javascript to clear the bar without the browser having to send a request

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,50 +1,89 @@
 import React, { useState } from 'react'
 
 const ExpenseForm = () => {
-    // const [enteredTitle, setEnteredTitle] = useState('')
-    // const [enteredAmount, setEnteredAmount] = useState('');
-    // const [enteredDate, setEnteredDate] = useState('')
+    const [enteredTitle, setEnteredTitle] = useState('');
+    const [enteredAmount, setEnteredAmount] = useState('');
+    const [enteredDate, setEnteredDate] = useState('');
 
-    const [userInput, setUserInput] = useState({
-        enteredTitle: '',
-        enteredAmount: '',
-        enteredDate: ''
-    })
+    // Used as one state object managed as one piece of state instead of 3 seperate slices like above
+    // now you have to update all 3 properties not just one when updating this state
+        // const [userInput, setUserInput] = useState({
+        //     enteredTitle: '',
+        //     enteredAmount: '',
+        //     enteredDate: ''
+        // })
 
     const titleChangeHandler = (event) => {
-        // setEnteredTitle(event.target.value);
-        setUserInput({
-            ...userInput,
-            enteredTitle:event.target.value,
-        })
+        setEnteredTitle(event.target.value);
+
+        // WHENEVER YOU UPDATE STATE AND DEPEND ON PREV STATE YOU SHOULD NOT DO IT LIKE THIS USE A FUNCTION
+
+        // setUserInput({
+        //     ...userInput,
+        //     enteredTitle:event.target.value,
+        // })
+
+        // With this approach it will be automatically executed by react and will receive prev state snapshot for that state which
+        // you are calling the updating function
+        // guarantees you are on the latest state snapshot
+            // setUserInput((prevState) => {
+                // return new state snapshot
+            //     return { ...prevState, enteredTitle: event.target.value };
+            // })
     };
 
     const amountChangeHandler = (event) => {
         setEnteredAmount(event.target.value);
+        // setUserInput({...userInput,
+        // enteredAmount: event.target.value,
+        // })
     };
 
     const dateChangeHandler = (event) => {
         setEnteredDate(event.target.value);
+        // setUserInput({
+        //     ...userInput,
+        //     enteredData: event.target.value,
+        // })
     };
 
-    return <form>
+    const submitHandler = (event) => {
+        // prevent default of page automatically sending request to prevent page from reloading when user submits form; keep handling
+        // with javascript
+        event.preventDefault();
+
+        const expenseData = {
+            title: enteredTitle,
+            amount: enteredAmount,
+            date: new Date(enteredDate)
+        }
+        console.log(expenseData);
+        // WHen form is submitted set back to empty string like initial state(override what user input after form is submitted and 
+        // clear input in input bar)
+        setEnteredTitle('');
+        setEnteredAmount('');
+        setEnteredDate('');
+    };
+
+    // listen to submit using onSubmit
+    return <form onSubmit={submitHandler}>
         <div className="new-expense__controls">
             <div className="new-expense__control">
                 <label>Title</label>
                 {/* onChange checks for whenever something in the input bar changes */}
-                <input type="text" onChange={titleChangeHandler}/>
+                <input type="text" value={enteredTitle} onChange={titleChangeHandler}/>
             </div>
         </div>
         <div className="new-expense__controls">
             <div className="new-expense__control">
                 <label>Amount</label>
-                <input type="number" min="0.01" step="0.01" onChange={amountChangeHandler}/>
+                <input type="number" min="0.01" step="0.01" value={enteredAmount} onChange={amountChangeHandler}/>
             </div>
         </div>
         <div className="new-expense__controls">
             <div className="new-expense__control">
                 <label>Date</label>
-                <input type="date" min="2019-01-01" max="2023-12-31" onChange={dateChangeHandler}/>
+                <input type="date" min="2019-01-01" max="2023-12-31" value={enteredDate} onChange={dateChangeHandler}/>
             </div>
         </div>
         <div className="new-expense__actions">
--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -3,8 +3,18 @@ import ExpenseForm from './ExpenseForm';
 import './NewExpense.css';
 
 const NewExpense = () => {
+    const saveExpenseDataHandler = (enteredExpenseData) => {
+        const expenseData = {
+             // copy all the entered expenseData which will be from the submitHandler in ExpenseForm.js
+            ...enteredExpenseData,
+            // Create an id using random and turn it into a string
+            id: Math.random().toString()
+        }
+    }
     return <div className="new-expense">
-        <ExpenseForm />
+        {/* pass new prop to expense form use "on" bc want to make it clear that it should be a function that cam be called inside
+        the expense form and something should happen inside of this component */}
+        <ExpenseForm onSaveExpenseData = {saveExpenseDataHandler}/>
     </div>
 };
 
